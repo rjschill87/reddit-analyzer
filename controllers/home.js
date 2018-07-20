1422070e94dc07432111123a16efe53272916da9
Robert Schillinger <schillinger.rob@gmail.com>
2018-07-20 11:42:08 -0500

    Testing reddit API

--- controllers/home.js
+++ controllers/home.js
@@ -2,8 +2,30 @@
  * GET /
  * Home page.
  */
+
+const request = require('request');
+
 exports.index = (req, res) => {
-  res.render('home', {
-    todo: 'Build it.'
+  getSubreddit('all', (err, postTitles) => {
+    if (err) return;
+
+    res.render('home', {
+      pageTitle: 'POSTS',
+      titles: postTitles
+    });
   });
 };
+
+function getSubreddit(sub, callback) {
+  const url = `https://www.reddit.com/r/${sub}.json`;
+
+  request.get(url, (err, res, body) => {
+    if (err) return;
+
+    const parsedBody = JSON.parse(body);
+    const posts = parsedBody.data.children;
+    const postTitles = posts.map(post => post.data.title);
+
+    callback(null, postTitles);
+  });
+}
